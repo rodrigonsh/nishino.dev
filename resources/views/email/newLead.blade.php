55646d2890b311f188cbdd153e1d0224e1b89223
Rodrigo Nishino <rodrigo.nsh@gmail.com>
2023-11-10 01:44:49 -0400

    edit newLead.blade

--- resources/views/email/newLead.blade.php
+++ resources/views/email/newLead.blade.php
@@ -1,7 +1,11 @@
 <h1>Novo LEAD pelo site!</h1>
 
+<p>CTA: <strong>{{ $lead->cta }}</strong></p>
+<br />
 <p>Nome: <strong>{{ $lead->name }}</strong></p>
 <p>Whatsapp: <strong>{{ $lead->whatsapp }}</strong></p>
 <p>email: <strong>{{ $lead->email }}</strong></p>
-<p>Orcamento: <strong>{{ $lead->orcamento }}</strong></p>
 <p>Endereço: <strong>{{ $lead->address }}</strong></p>
+<br />
+<p>Body: <strong>{{ $lead->body }}</strong></p>
+<p>Orcamento: <strong>{{ $lead->orcamento }}</strong></p>
