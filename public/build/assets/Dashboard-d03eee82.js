79b7ddef9c5f42fb7bc0e880d29cac1a77734917
Rodrigo Nishino <rodrigo.nsh@gmail.com>
2023-11-17 10:40:07 -0400

    porra

--- public/build/assets/Dashboard-d03eee82.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./AuthenticatedLayout-cc718279.js";import{o as t,h as r,a,u as s,w as o,F as m,X as _,d as c}from"./app-ef9ccb1a.js";const i={__name:"Dashboard",setup(n){return(l,u)=>(t(),r(m,null,[a(s(_),{title:"Dashboard"}),a(e,{title:"DASHBOARD"},{default:o(()=>[c(" HUmmm ")]),_:1})],64))}};export{i as default};
